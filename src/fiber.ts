d6a2174c8d5911db64aae32767d1408719741b75
ion-project <gabrielcursino.medeiros@gmail.com>
2023-06-20 19:09:59 -0300

    fix: export router

--- src/fiber.ts
+++ src/fiber.ts
@@ -1,5 +1,6 @@
 import { App } from './app';
 import { HttpRequest } from './http-request';
 import { HttpResponse } from './http-response';
+import { Router } from './router';
 
-export { App, HttpRequest, HttpResponse }
+export { App, HttpRequest, HttpResponse, Router }
