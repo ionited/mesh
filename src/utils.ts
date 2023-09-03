9173599cbf4a50687510ef54fa9368aa209fbcd6
hamdallahjodah <139851401+hamdallahjodah@users.noreply.github.com>
2023-09-03 22:59:32 +0300

    Update utils.ts

--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,17 @@
 export const parseQuery = (query: string) => query.split('&').reduce((data, q) => {
   const parts = q.split('=');
+  const key = decodeURIComponent(parts[0]);
+  const value = decodeURIComponent(parts[1]);
 
-  data[decodeURIComponent(parts[0])] = decodeURIComponent(parts[1]);
+  if (key.endsWith('[]')) {
+    const normalizedKey = key.slice(0, -2); // Remove the '[]' suffix
+    if (!data[normalizedKey]) {
+      data[normalizedKey] = [];
+    }
+    data[normalizedKey].push(value);
+  } else {
+    data[key] = value;
+  }
 
   return data;
-}, {} as { [key: string]: string });
+}, {} as { [key: string]: string | string[] });
